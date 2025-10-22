376213cf09d6e78ea88c9612915e9a9d39c2317e
az-maa <aandadiasmaa@gmail.com>
2025-10-22 22:31:32 +0100

    Initial Spring Boot project setup

---
+++ java-springboot/social-media-rest-api/src/test/java/com/example/socialmediarestapi/SocialMediaRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.socialmediarestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialMediaRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
