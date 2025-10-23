5f70a4087c6979ceb8f7ba4b080b7ef5b0785800
az-maa <aandadiasmaa@gmail.com>
2025-10-23 21:22:12 +0100

    changing names

--- java-springboot/social-media-rest-api/src/main/java/com/example/socialmediarestapi/Model/ListResponse.java
+++ java-springboot/social-media-rest-api/src/main/java/com/example/socialmediarestapi/Model/ListResponse.java
@@ -1,4 +1,53 @@
 package com.example.socialmediarestapi.Model;
 
-public class FileResponse {
+import java.util.List;
+public class ListResponse<T> {
+    private List<T> data;
+    private int total;
+    private int page;
+    private int limit;
+
+    // Constructors
+    public ListResponse() {}
+
+    public ListResponse(List<T> data, int total, int page, int limit) {
+        this.data = data;
+        this.total = total;
+        this.page = page;
+        this.limit = limit;
+    }
+
+    // Getters and Setters
+    public List<T> getData() {
+        return data;
+    }
+
+    public void setData(List<T> data) {
+        this.data = data;
+    }
+
+    public int getTotal() {
+        return total;
+    }
+
+    public void setTotal(int total) {
+        this.total = total;
+    }
+
+    public int getPage() {
+        return page;
+    }
+
+    public void setPage(int page) {
+        this.page = page;
+    }
+
+    public int getLimit() {
+        return limit;
+    }
+
+    public void setLimit(int limit) {
+        this.limit = limit;
+    }
 }
+
