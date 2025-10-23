9a5b6ff8ef48848adced3b385f0513c0d9bc4863
az-maa <aandadiasmaa@gmail.com>
2025-10-23 21:41:13 +0100

    local changes

--- java-springboot/social-media-rest-api/src/main/java/com/example/socialmediarestapi/Controller/ListController.java
+++ java-springboot/social-media-rest-api/src/main/java/com/example/socialmediarestapi/Controller/ListController.java
@@ -1,4 +1,14 @@
 package com.example.socialmediarestapi.Controller;
 
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
 public class ListController {
+
+    @GetMapping("/placeholder")
+    public String placeholder() {
+        return "API is working!";
+    }
 }
